722f24a2ff326f5bc52bcad4e13c067cdbcd8346
Shlok Prajapati <shlokprajapati2003@gmail.com>
2023-04-30 17:09:15 +0530

    made responsive

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,7 +6,7 @@ import { Instagram, LinkedIn, YouTube } from '@mui/icons-material'
 
 export default function Footer() {
     return (
-        <footer className="absolute bottom-0 w-full max-w-full bg-footer flex flex-col justify-center rounded-t-[3.5rem] pb-5 shadow-[0px_-13px_2px_0px_#00000040] md:pb-0 md:flex-row md:justify-around md:rounded-t-[7.25rem]">
+        <footer className="bottom-0 mt-auto max-w-full bg-footer flex flex-col justify-center rounded-t-[3.5rem] pb-5 shadow-[0px_-13px_2px_0px_#00000040] md:pb-0 md:flex-row md:justify-around md:rounded-t-[7.25rem]">
             <div className="grid grid-cols-3 gap-2 sm:gap-4 text-center pt-8 sm:pt-16 pb-5 md:pb-16 px-5 sm:my-auto md:gap-x-8 md:gap-y-6">
                 <Link to="/" className="sm:border-2 sm:border-black sm:rounded-lg my-auto sm:py-1 sm:px-1.5 text-xs sm:text-base md:text-lg">Events</Link>
                 <Link to="/" className="sm:border-2 sm:border-black sm:rounded-lg my-auto sm:py-1 sm:px-1.5 text-xs sm:text-base md:text-lg">Vision/Missions</Link>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,27 +2,34 @@ import React from "react"
 
 import Logo from "../assets/home/logo.png"
 
+function Landing() {
+    return (
+        <div className="h-64 min-[360px]:h-60 sm:h-72 md:h-[17rem] lg:h-80 bg-home rounded-b-[76px] md:rounded-b-[94px] lg:rounded-b-[114px]">
+            <div className="relative top-28 flex flex-row justify-around items-center mx-2">
+
+                <span className="uppercase font-bold text-lg min-[360px]:text-xl min-[500px]:text-2xl sm:text-3xl md:text-4xl lg:text-5xl">
+                    <h1 className="text-[#000778]">Welcome to the</h1>
+                    <h1 className="text-[#000778]">Official Page of</h1>
+                    <h1 className="text-transparent bg-clip-text bg-gradient-to-r from-[#030B98] from-[16.16%] to-[#FF9201] to-[43.13%] 
+                                text-xl min-width[360px]:text-2xl min-[500px]:text-4xl sm:text-5xl md:text-6xl lg:text-7xl">Coding Club</h1>
+                </span>
+
+                <span className="">
+                    <img src={Logo} className="inline-block mx-auto h-14 w-28 min-[500px]:h-20 min-[500px]:w-40 sm:h-24 sm:w-48 lg:h-28 lg:w-56 xl:h-36 xl:w-72" />
+                    <h1 className="mx-auto text-center text-lg sm:text-xl md:text-2xl">
+                        <span className="tracking-wider">TCET's शSTRA</span>
+                    </h1>
+                </span>
+
+            </div>
+        </div>
+    )
+}
+
 export default function Home() {
     return (
-        <div className="relative -z-10">
-            <div className="h-60 sm:h-72 md:h-[17rem] lg:h-80 w-full bg-home rounded-b-[76px] md:rounded-b-[94px] lg:rounded-b-[114px]">
-                <div className="relative top-28 flex flex-row justify-around items-center mx-2">
-
-                    <span className="uppercase font-bold text-xl min-[500px]:text-2xl sm:text-3xl md:text-4xl lg:text-5xl">
-                        <h1 className="text-[#000778]">Welcome to the</h1>
-                        <h1 className="text-[#000778]">Official Page of</h1>
-                        <h1 className="text-transparent bg-clip-text bg-gradient-to-r from-[#030B98] from-[16.16%] to-[#FF9201] to-[43.13%] text-2xl min-[500px]:text-4xl sm:text-5xl md:text-6xl lg:text-7xl">Coding Club</h1>
-                    </span>
-
-                    <span className="">
-                        <img src={Logo} className="inline-block mx-auto h-14 w-28 min-[500px]:h-20 min-[500px]:w-40 sm:h-24 sm:w-48 lg:h-28 lg:w-56 xl:h-36 xl:w-72" />
-                        <h1 className="mx-auto text-center text-lg sm:text-xl md:text-2xl">
-                            <span className="tracking-wider">TCET's शSTRA</span>
-                        </h1>
-                    </span>
-
-                </div>
-            </div>  
+        <div className="relative -z-10 min-h-[80%]">
+            <Landing />
         </div>
     )
 }

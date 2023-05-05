a20e5655c301c310a51cffc1af70eb296243464b
Shlok Prajapati <shlokprajapati2003@gmail.com>
2023-05-05 11:09:00 +0530

    made ui responsive

--- src/components/home/CorePeeps.jsx
+++ src/components/home/CorePeeps.jsx
@@ -13,7 +13,7 @@ export default function CorePeeps() {
     return (
         <div className="flex flex-col self-center">
             <span className="pb-7 self-center  uppercase font-bold text-2xl md:text-4xl select-none inline-block w-max gradient-txt">Core Peeps</span>
-            <div className="grid grid-cols-2 sm:grid-c lg:grid-cols-4 gap-4 sm:gap-8 md:gap-10 lg:gap-x-20 lg:gap-y-10 md:mx-2">
+            <div className="grid grid-cols-2 sm:grid-cols-4 gap-6 gap-x-10 sm:gap-x-8 sm:gap-8 md:gap-10 lg:gap-x-20 lg:gap-y-10 md:mx-2">
                 {    
                     core &&
                     core.map(personImg => (
@@ -22,7 +22,7 @@ export default function CorePeeps() {
                             className="flex flex-col items-center aspect-square justify-center space-y-2"
                         >
                             <span className="uppercase font-bold text-lg sm:text-2xl md:text-3xl lg:text-4xl text-center mx-auto inline-block w-max gradient-txt">post</span>
-                            <img src={personImg} className="inline-block aspect-square h-16 min-[500px]:h-20 sm:h-28 md:h-36 lg:h-60"/>
+                            <img src={personImg} className="inline-block aspect-square h-28 sm:h-28 md:h-36 lg:h-60"/>
                         </div>
                     ))
                 }

376b7278c4f38f4ca976129707e81ee31cab7b0c
Shlok Prajapati <shlokprajapati2003@gmail.com>
2023-05-15 23:22:15 +0530

    Made changes in TeamCard"

--- src/components/Card/TeamCard.jsx
+++ src/components/Card/TeamCard.jsx
@@ -6,11 +6,11 @@ export default function TeamCard(props) {
     return (
         <div
             id={props.id}
-            className="aspect-auto max-h-64 w-32 md:w-40 lg:w-52 rounded-xl mx-auto my-auto col-span-2 bg-gray-300 overflow-hidden border border-black"
+            className="aspect-auto max-h-64 w-32 md:w-40 lg:w-52 rounded-xl mx-auto my-auto col-span-2 bg-gray-300 border border-black"
         >
             <div className="linkedin-container relative min-h-[8rem] md:min-h-[10rem] overflow-hidden">
                 {/* You can add padding to the image as per your requirements */}
-                <img src={props.image} className="inline-block max-h-40 md:max-h-52 w-full object-cover object-center rounded-xl " alt="core image"/>
+                <img src={props.image} className="inline-block max-h-40 md:max-h-52 w-full object-fill object-center" alt="core image"/>
                 <div className="linkedin absolute z-20 opacity-100 -top-[13rem] w-full h-full flex justify-center items-center bg-gradient-to-br from-[#ffffff14] to-[#ffffff00] backdrop-blur-[10px]">
                     <Link 
                         to={props.linkedin} 
